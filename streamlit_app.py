99f684180b5738097e8b2e47da04b8c3af3cc502
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-10 02:22:49 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,136 @@
+# gpt3 professional email generator by stefanrmmr - version June 2022
 
-    
-import streamlit as st
+import os
 import openai
+import streamlit as st
+
+# DESIGN implement changes to the standard streamlit UI/UX
+st.set_page_config(page_title="rephraise", page_icon="img/rephraise_logo.png",)
+# Design move app further up and remove top padding
+st.markdown('''<style>.css-1egvi7u {margin-top: -4rem;}</style>''',
+    unsafe_allow_html=True)
+# Design change hyperlink href link color
+st.markdown('''<style>.css-znku1x a {color: #9d03fc;}</style>''',
+    unsafe_allow_html=True)  # darkmode
+st.markdown('''<style>.css-znku1x a {color: #9d03fc;}</style>''',
+    unsafe_allow_html=True)  # lightmode
+# Design change height of text input fields headers
+st.markdown('''<style>.css-qrbaxs {min-height: 0.0rem;}</style>''',
+    unsafe_allow_html=True)
+# Design change spinner color to primary color
+st.markdown('''<style>.stSpinner > div > div {border-top-color: #9d03fc;}</style>''',
+    unsafe_allow_html=True)
+# Design change min height of text input box
+st.markdown('''<style>.css-15tx938{min-height: 0.0rem;}</style>''',
+    unsafe_allow_html=True)
+# Design hide top header line
+hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
+# Design hide "made with streamlit" footer menu area
+hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}</style>"""
+st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 
 
+def gen_mail_contents(email_contents):
 
-# Define the fields
-fields = ["Project Title", "Subject", "Area"]
+    # iterate through all seperate topics
+    for topic in range(len(email_contents)):
+        input_text = email_contents[topic]
+        rephrased_content = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"Continue talking about the topics and try to introduce the next point called Convolutional Networks and Keras",
+            # prompt=f"Rewrite the text to sound professional, elaborate and polite.\nText: {input_text}\nRewritten text:",
+            temperature=0.8,
+            max_tokens=len(input_text)*3,
+            top_p=0.8,
+            best_of=2,
+            frequency_penalty=0.0,
+            presence_penalty=0.0)
 
-# Initialize the form data
-form_data = {"Project Title": "", "Subject": "", "Area": ""}
+        # replace existing topic text with updated
+        email_contents[topic] = rephrased_content.get("choices")[0]['text']
+    return email_contents
 
-# Create a function to generate the content for a field
-def generate_field(field):
-    # Prompt the user for input
-    prompt = "Enter the content for the " + field + " field:"
-    content = st.text_input(prompt)
+
+def gen_mail_format(sender, recipient, style, email_contents):
+    # update the contents data with more formal statements
+    email_contents = gen_mail_contents(email_contents)
+    # st.write(email_contents)  # view augmented contents
+
+    contents_str, contents_length = "", 0
+    for topic in range(len(email_contents)):  # aggregate all contents into one
+        contents_str = contents_str + f"\nContent{topic+1}: " + email_contents[topic]
+        contents_length += len(email_contents[topic])  # calc total chars
+
+    email_final_text = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Im in the last course of Coumputer Sciences and I am writing my final proyect which title is CNNs & AI systems to detect cancer an I want you to write the Introduction. Pease put the citation in APA format at the end. Your answer needs to have a  minimun of 800 words. Give me references at the end \n",
+        # prompt=f"Write a professional sounding email text that includes all of the following contents separately.\nThe text needs to be written to adhere to the specified writing styles and abbreviations need to be replaced.\n\nSender: {sender}\nRecipient: {recipient} {contents_str}\nWriting Styles: motivated, formal\n\nEmail Text:",
+        temperature=0.8,
+        max_tokens=1000,
+        top_p=0.8,
+        best_of=2,
+        frequency_penalty=0.0,
+        presence_penalty=0.0)
+
+    return email_final_text.get("choices")[0]['text']
+
+
+def main_gpt3emailgen():
+
+    st.image('img/image_banner.png')  # TITLE and Creator information
+    st.markdown('Generate Accurate & Quality Projects Degree in Just Seconds and get Full Marks This Course')
+    st.write('\n')  # add spacing
+
+    st.subheader('\nWhat is your introduction all about?\n')
+    with st.expander("SECTION - Project Detail Inputs", expanded=True):
+
+        input_c1 = st.text_input('Enter relevant information about the topic down below! (currently 2x seperate topics supported)', 'University Degree')
+        input_c2 = st.text_input('', 'Title of the Project')
+
+        email_text = ""  # initialize columns variables
+        col1, col2, col3, space, col4 = st.columns([5, 5, 5, 0.5, 5])
+        with col1:
+            input_sender = st.text_input('N.Pages', '60')
+        with col2:
+            input_recipient = st.text_input('Font', 'Arial')
+            
+        with col3:
+            input_style = st.selectbox('Citation format',
+                                       ('APA', 'IEEE', 'Harvard'),
+                                       index=0)
+        with col4:
+            st.write("\n")  # add spacing
+            st.write("\n")  # add spacing
+            if st.button('Generate Project'):
+                with st.spinner():
+
+                    input_contents = []  # let the user input all the data
+                    if (input_c1 != "") and (input_c1 != 'topic 1'):
+                        input_contents.append(str(input_c1))
+                    if (input_c2 != "") and (input_c2 != 'topic 2 (optional)'):
+                        input_contents.append(str(input_c2))
+
+                    if (len(input_contents) == 0):  # remind user to provide data
+                        st.write('Please fill in some contents for your message!')
+                    if (len(input_sender) == 0) or (len(input_recipient) == 0):
+                        st.write('Sender and Recipient names can not be empty!')
+
+                    if (len(input_contents) >= 1):  # initiate gpt3 mail gen process
+                        if (len(input_sender) != 0) and (len(input_recipient) != 0):
+                            email_text = gen_mail_format(input_sender,
+                                                         input_recipient,
+                                                         input_style,
+                                                         input_contents)
+    if email_text != "":
+        st.write('\n')  # add spacing
+        st.subheader('\nYou sound incredibly professional!\n')
+        with st.expander("SECTION - Email Output", expanded=True):
+            st.markdown(email_text)  #output the results
+
+if __name__ == '__main__':
+    # call main function
+    main_gpt3emailgen()
     
-    # Use OpenAI to generate content for the field
-    model_engine = "text-davinci-002"
-    completions = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-
-    message = completions.choices[0].text
-    content += message
-    return content
-
-# Create a function to download the content as a PDF
-def download_pdf(content):
-    # TODO: Write the code to convert the content to a PDF and download it
-    pass
-
-# Main function to build the form
-def main():
-    # Add a title for the form
-    st.title("Academic Project Generator")
     
-    # Add a form for the user to enter the project details
-    form_data["Project Title"] = st.text_input("Project Title")
-    form_data["Subject"] = st.text_input("Subject")
-    form_data["Area"] = st.text_input("Area")
-    
-    # Add a button to generate the content for each field
-    for field in fields:
-        st.write("")
-        if st.button("Generate " + field):
-            form_data[field] = generate_field(field)
-    
-    # Display the generated content for each field
-    for field in fields:
-        st.write("")
-        st.write("### " + field)
-        st.write(form_data[field])
-    
-    # Add a button to download the content as a PDF
-    if st.button("Download PDF"):
-        content = "\n".join([field + ": " + form_data[field] for field in fields])
-        download_pdf(content)
-
-# Run the form
-if _name_ == "_main_":
-    main()
