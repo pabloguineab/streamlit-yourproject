f3a39c6e08b375c695bda5fceb36f4fe25a09dde
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-10 12:26:41 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def gen_mail_contents(email_contents):
         input_text = email_contents[topic]
         rephrased_content = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=f"Write an introduction for a research paper with the title  High Rate Unployment in Spain, The introduction should provide an overview of the research topic, its significance, and a summary of the contents in the paper and need to has minimum 7 paragraphs.",
+            prompt=f"Write an introduction for a research paper with the title {input_text}, The introduction should provide an overview of the research topic, its significance, and a summary of the contents in the paper and need to has minimum 7 paragraphs.",
             # prompt=f"Rewrite the text to sound professional, elaborate and polite.\nText: {input_text}\nRewritten text:",
             temperature=0.8,
             max_tokens=len(input_text)*5,
