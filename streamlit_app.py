9d369687188b7d9fa85d46d4d26d6917e42aa91d
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-09 23:12:46 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,7 @@ hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
 st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 
 
-# Connect to OpenAI GPT-3, fetch API key from Streamlit secrets
-openai.api_key = os.getenv("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")
+openai.api_key = "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"
 
 
 def gen_mail_contents(email_contents):
