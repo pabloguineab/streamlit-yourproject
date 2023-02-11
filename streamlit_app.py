b0e95c07e88311155ee4e16c957382b30ea923d1
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-11 08:07:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,10 @@ from streamlit_extras.stoggle import stoggle
 
 
 # DESIGN implement changes to the standard streamlit UI/UX
-st.set_page_config(page_title="YourProject.ai", page_icon="img/rephraise_logo.png",)
-
+st.set_page_config(page_title="rephraise", page_icon="img/rephraise_logo.png",)
+# Design move app further up and remove top padding
+st.markdown('''<style>.css-1egvi7u {margin-top: -4rem;}</style>''',
+    unsafe_allow_html=True)
 # Design change hyperlink href link color
 st.markdown('''<style>.css-znku1x a {color: #9d03fc;}</style>''',
     unsafe_allow_html=True)  # darkmode
@@ -27,11 +29,13 @@ st.markdown('''<style>.stSpinner > div > div {border-top-color: #9d03fc;}</style
 st.markdown('''<style>.css-15tx938{min-height: 0.0rem;}</style>''',
     unsafe_allow_html=True)
 # Design hide top header line
-
+hide_decoration_bar_style = '''<style>header {visibility: hidden;}</style>'''
+st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 # Design hide "made with streamlit" footer menu area
 hide_streamlit_footer = """<style>#MainMenu {visibility: hidden;}
                         footer {visibility: hidden;}</style>"""
 st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
+
 
 
 def gen_mail_contents(email_contents):
