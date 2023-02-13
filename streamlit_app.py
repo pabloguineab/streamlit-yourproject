3a7b3d0a43c74a1c4690087e0756c21208b67ff8
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-13 09:48:32 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ def gen_mail_format(sender, recipient, style, email_contents):
         prompt=f"Write an introduction for a research paper with the title  {contents_str}, The introduction should provide an overview of the research topic, its significance, and a summary of the contents in the paper and need to has minimum 7 paragraphs. After the introduction, the rest of the sections of the project are developed in detail.\n",
         # prompt=f"Write a professional sounding email text that includes all of the following contents separately.\nThe text needs to be written to adhere to the specified writing styles and abbreviations need to be replaced.\n\nSender: {sender}\nRecipient: {recipient} {contents_str}\nWriting Styles: motivated, formal\n\nEmail Text:",
         temperature=0.9,
-        max_tokens= 3000,
+        max_tokens= 9000,
         top_p=0.8,
         best_of=1,
         frequency_penalty=0.0,
