3f63f489ec53617b9215da247908d50a66f2f168
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-26 17:21:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ from fpdf import FPDF
 import base64
 from streamlit_extras.stoggle import stoggle
 
-
 # DESIGN implement changes to the standard streamlit UI/UX
 st.set_page_config(page_title="rephraise", page_icon="img/rephraise_logo.png",)
 # Design move app further up and remove top padding
@@ -38,15 +37,15 @@ st.markdown(hide_streamlit_footer, unsafe_allow_html=True)
 
 
 
-def gen_mail_contents(email_contents):
 
-    # iterate through all seperate topics
-    for topic in range(len(email_contents)):
-        input_text = email_contents[topic]
+def gen_project_contents(project_contents):
+
+    # iterate through all separate sections
+    for section in range(len(project_contents)):
+        input_text = project_contents[section]
         rephrased_content = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=f"Write an introduction for a research paper with the title {input_text}, The introduction should provide an overview of the research topic, its significance, and a summary of the contents in the paper and need to has minimum 7 paragraphs. The keywords of the project are {input_text}. After writing the introduction, continue developing the rest of the sections of the project.",
-            # prompt=f"Rewrite the text to sound professional, elaborate and polite.\nText: {input_text}\nRewritten text:",
+            prompt=f"Write a section of an academic project on the topic {input_text}, with the title '{project_contents[0]}'. The section should discuss the topic and its relevance to the project, and it should have at least 5 paragraphs.",
             temperature=0.8,
             max_tokens=len(input_text)*5,
             top_p=0.8,
@@ -54,85 +53,62 @@ def gen_mail_contents(email_contents):
             frequency_penalty=0.0,
             presence_penalty=0.0)
 
-        # replace existing topic text with updated
-        email_contents[topic] = rephrased_content.get("choices")[0]['text']
-    return email_contents
+        # replace existing section text with updated
+        project_contents[section] = rephrased_content.get("choices")[0]['text']
+    return project_contents
 
 
-
-
-def gen_mail_format(sender, recipient, style, email_contents):
-    # update the contents data with more formal statements
-    email_contents = gen_mail_contents(email_contents)
-    # st.write(email_contents)  # view augmented contents
+def gen_project_format(title, sections):
+    # update the sections data with more formal statements
+    sections = gen_project_contents(sections)
+    # st.write(sections)  # view augmented contents
 
     contents_str, contents_length = "", 0
-    for topic in range(len(email_contents)):  # aggregate all contents into one
-        contents_str = contents_str + f"\nContent{topic+1}: " + email_contents[topic]
-        contents_length += len(email_contents[topic])  # calc total chars
+    for section in range(len(sections)):  # aggregate all sections into one
+        contents_str = contents_str + f"\nSection {section+1}: " + sections[section]
+        contents_length += len(sections[section])  # calc total chars
 
-    email_final_text = openai.Completion.create(
+    project_final_text = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"Write an introduction for a research paper with the title  {contents_str}, The introduction should provide an overview of the research topic, its significance, and a summary of the contents in the paper and need to has minimum 7 paragraphs. After the introduction, the rest of the sections of the project are developed in detail.\n",
-        # prompt=f"Write a professional sounding email text that includes all of the following contents separately.\nThe text needs to be written to adhere to the specified writing styles and abbreviations need to be replaced.\n\nSender: {sender}\nRecipient: {recipient} {contents_str}\nWriting Styles: motivated, formal\n\nEmail Text:",
+        prompt=f"Write an academic project with the title '{title}', consisting of the following sections:{contents_str}\n",
         temperature=0.6,
-        max_tokens= 3000,
+        max_tokens=3000,
         top_p=0.8,
         best_of=1,
         frequency_penalty=0.0,
         presence_penalty=0.0)
 
-    return email_final_text.get("choices")[0]['text']
+    return project_final_text.get("choices")[0]['text']
 
 
-def main_gpt3emailgen():
-
+def main_gpt3projectgen():
     st.image('img/image_banner.png')  # TITLE and Creator information
-    st.markdown('Generate Accurate & Quality Projects Degree in Just Seconds and get Full Marks This Course')
+    st.markdown('Generate Accurate & Quality Academic Projects in Just Seconds')
     st.write('\n')  # add spacing
 
-    st.subheader('\nWhat is your Project about?\n')
+    st.subheader('\nWhat is your project about?\n')
     with st.expander("SECTION - Project Detail Inputs", expanded=True):
+        input_title = st.text_input('Title of the project', 'My Project Title')
+        input_section1 = st.text_input('Section 1 (Introduction)', 'Introduction')
+        input_section2 = st.text_input('Section 2', '')
+        input_section3 = st.text_input('Section 3', '')
+        input_section4 = st.text_input('Section 4', '')
+        input_section5 = st.text_input('Section 5', '')
+        input_section6 = st.text_input('Section 6', '')
+        input_section7 = st.text_input('Section 7', '')
+        input_section8 = st.text_input('Section 8', '')
+        input_conclusion = st.text_input('Conclusion', 'Conclusion')
 
-        input_c1 = st.text_input('Enter relevant information about the topic down below! (currently 2x seperate topics supported)', 'Title of the Project')
-        input_c2 = st.text_input('Enter 10 keywords that you consider relevant to mention in the project', 'Keywords')
-        input_c3 = st.text_input('Copy and Paste the Abstract', 'Abstract or Summary')
+    sections = [input_title, input_section1, input_section2, input_section3, input_section4, input_section5, input_section6, input_section7, input_section8, input_conclusion]
 
-        email_text = ""  # initialize columns variables
-        col1, col2, col3, space, col4 = st.columns([5, 5, 5, 0.5, 5])
-        with col1:
-            input_sender = st.text_input('N.Pages', '60')
-        with col2:
-            input_recipient = st.text_input('Font', 'Arial')
-            
-        with col3:
-            input_style = st.selectbox('Citation format',
-                                       ('APA', 'IEEE', 'Harvard'),
-                                       index=0)
-        with col4:
-            st.write("\n")  # add spacing
-            st.write("\n")  # add spacing
-            if st.button('Generate Project'):
-                with st.spinner():
+    project_text = gen_project_format(input_title, sections)
 
-                    input_contents = []  # let the user input all the data
-                    if (input_c1 != "") and (input_c1 != 'Title of the Project'):
-                        input_contents.append(str(input_c1))
-                    if (input_c2 != "") and (input_c2 != 'Keywords'):
-                        input_contents.append(str(input_c2))
+    st.write('\n\n\n\n\n')  # add spacing
+    with st.expander("SECTION - Generated Text", expanded=True):
+        st.write(project_text)
 
-                    if (len(input_contents) == 0):  # remind user to provide data
-                        st.write('Please fill in some contents for your message!')
-                    if (len(input_sender) == 0) or (len(input_recipient) == 0):
-                        st.write('Sender and Recipient names can not be empty!')
 
-                    if (len(input_contents) >= 1):  # initiate gpt3 mail gen process
-                        if (len(input_sender) != 0) and (len(input_recipient) != 0):
-                            email_text = gen_mail_format(input_sender,
-                                                         input_recipient,
-                                                         input_style,
-                                                         input_contents)
-    if email_text != "":
+ if project_text != "":
         st.write('\n')  # add spacing
         st.subheader('\nDownload your Project\n')       
         pdf = FPDF()  # pdf object
@@ -142,7 +118,7 @@ def main_gpt3emailgen():
         pdf.set_font("Times")
         pdf.cell(60,10,'Introduction',0,1,'C');
         pdf.set_xy(20.0, 20.0)  # adjust x and y position to set the margins
-        pdf.multi_cell(w=170.0, h=5.0, align="L", txt=email_text)  # use multi_cell to wrap the text
+        pdf.multi_cell(w=170.0, h=5.0, align="L", txt=project_text)  # use multi_cell to wrap the text
         st.markdown(
             f'<a href={STRIPE_CHECKOUT} class="button">👉 Get Complete Project --> Proceed to payment</a>',
             unsafe_allow_html=True,
