f241425a24ad792c69ee746a3a8442f59b5e6556
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-27 10:16:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,13 +155,23 @@ def main_gpt3projectgen():
         st.markdown('### Project Preview:\n')
         st.write(project_final_text)
         st.text_area('Generated Text', value=project_final_text) # Show the entire generated text without scrolling
-        with open("project_final_text.pdf", "rb") as pdf_file:
-            PDFbyte = pdf_file.read()
 
-        st.download_button(label="Export_Report",
-                            data=PDFbyte,
-                            file_name="project_final_text.pdf",
-                            mime='application/octet-stream')
+
+        export_as_pdf = st.button("Download PDF)
+
+        def create_download_link(val, filename):
+            b64 = base64.b64encode(val)  # val looks like b'...'
+            return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+
+        if export_as_pdf:
+            pdf = FPDF()
+            pdf.add_page()
+            pdf.set_font('Arial', 'B', 16)
+            pdf.cell(40, 10, project_final_text)
+
+            html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+
+            st.markdown(html, unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
