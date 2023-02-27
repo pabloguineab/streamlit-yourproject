311af93b34da1791607ce101a023b869377a9885
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-02-27 12:06:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,27 +161,27 @@ def main_gpt3projectgen():
         st.markdown('### Project Preview:\n')
         st.write(project_final_text)
         st.text_area('Generated Text', value=project_final_text) # Show the entire generated text without scrolling
-
+        if st.button('Download Project'):
         # Generate PDF
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_xy(0, 0)
-        pdf.set_font('arial', 'B', 13.0)
-        pdf.cell(ln=0, h=5.0, align='L', w=0, txt=input_title, border=0)
-        pdf.ln(20)
-        pdf.set_font('arial', '', 13.0)
-        pdf.multi_cell(0, 5, txt=project_final_text, border=0, align='L')
-        # Generate PDF file
-        # Create a memory file
-        mem_file = io.BytesIO()
-        # Write pdf to file
-        pdf.output(mem_file, 'F')
-        # Get value of the BytesIO object
-        project_file = mem_file.getvalue()
-        encoded_project_file = base64.b64encode(project_file)  # encode as base64
-        href = f'<a href="data:application/octet-stream;base64,{encoded_project_file.decode()}" download="project.pdf">Download Project as PDF</a>'
-        st.markdown(href, unsafe_allow_html=True)
-        st.success('\nProject PDF Generated!')
+            pdf = FPDF()
+            pdf.add_page()
+            pdf.set_xy(0, 0)
+            pdf.set_font('arial', 'B', 13.0)
+            pdf.cell(ln=0, h=5.0, align='L', w=0, txt=input_title, border=0)
+            pdf.ln(20)
+            pdf.set_font('arial', '', 13.0)
+            pdf.multi_cell(0, 5, txt=project_final_text, border=0, align='L')
+            # Generate PDF file
+            # Create a memory file
+            mem_file = io.BytesIO()
+            # Write pdf to file
+            pdf.output(mem_file, 'F')
+            # Get value of the BytesIO object
+            project_file = mem_file.getvalue()
+            encoded_project_file = base64.b64encode(project_file)  # encode as base64
+            href = f'<a href="data:application/octet-stream;base64,{encoded_project_file.decode()}" download="project.pdf">Download Project as PDF</a>'
+            st.markdown(href, unsafe_allow_html=True)
+            st.success('\nProject PDF Generated!')
 
 if __name__ == '__main__':
     main_gpt3projectgen()
